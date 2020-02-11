697829a32a957e99b53de76118d11eeafe31207e
Filipe de Leonel Batista <filipe.batista@salux.local>
2020-02-11 19:05:29 -0300

    adicionando alteracoes automatizado

--- category.php
+++ category.php
@@ -1,53 +1,49 @@
 <?php get_header(); ?>
 <div class="container">
- 
-<?php 
-// Check if there are any posts to display
-if ( have_posts() ) : ?>
- 
-<header class="archive-header">
-<h1 class="archive-title"><?php single_cat_title( '', false ); ?></h1>
- 
+
  <?php
-// Display optional category description
- if ( category_description() ) : ?>
-<div class="archive-meta">
-    <h3><?php echo category_description(); ?></h3>
-</div>
-<hr>
-<?php endif; ?>
-</header>
- 
-<?php
- 
-// The Loop
-while ( have_posts() ) : the_post(); ?>
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+    $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => 5,
+        'paged' => $paged
+    );          
+    
+    $the_query = new WP_Query( $args );?>
 
+    <?php if( $the_query->have_posts() ) : ?>
+        <h1>Notícias Ascorsan</h1>
+        <hr>
+           <?php while( $the_query->have_posts() ) : 
+                $the_query->the_post();  ?>   
 
-<div class="col-sm-12 mb-3">
-        <div class="row d-flex align-items-center">
-            <div class="col-sm-4">
-                <?php the_post_thumbnail('post-thumbnail', array(
-                    'class' => 'img-thumbnail',
-                    'style' => 'height: 170px; width: 100%; display: block;',
-                )); ?>
-            </div>
-            <div class="col-sm-8">
-                <a href="<?php the_permalink(); ?>" class="titulo-link"><h3><?php the_title(); ?></h3></a>
-                <p><?php the_excerpt(); ?></p>
-                <div class="col-sm mb-2">                    
-                    <a href="<?php the_permalink(); ?>">Continue lendo</a>
-                </div>
-                <div class="mb-1 text-muted"><i class="far fa-clock"></i><small>Publicado em: <?php echo get_the_date('d/m/Y'); ?> </small></div>
-            </div>
-        </div>
-    </div>
+                <div class="col-sm-12 mb-3">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-sm-4">
+                                <?php the_post_thumbnail('post-thumbnail', array(
+                                    'class' => 'img-thumbnail',
+                                    'style' => 'height: 170px; width: 100%; display: block;',
+                                )); ?>
+                            </div>
+                            <div class="col-sm-8">
+                                <a href="<?php the_permalink(); ?>" class="titulo-link"><h3><?php the_title(); ?></h3></a>
+                                <p><?php the_excerpt(); ?></p>
+                                <div class="col-sm mb-2">                    
+                                    <a href="<?php the_permalink(); ?>">Continue lendo</a>
+                                </div>
+                                <div class="mb-1 text-muted"><i class="far fa-clock"></i><small>Publicado em: <?php echo get_the_date('d/m/Y'); ?> </small></div>
+                            </div>
+                        </div>
+                    </div>
  
-<?php endwhile; ?>
- 
-
-<?php else :
-    get_template_part('no_results');
+            <?php endwhile; ?>
+            <div class="row ml-3 mb-4">
+                <?php if (function_exists("pagination")) {
+                    pagination($the_query->max_num_pages);
+                } ?>  
+            </div> 
+    <?php else :
+        get_template_part('no_results');
     endif; ?>
 
 <?php get_template_part('ultimas-noticias'); ?>
