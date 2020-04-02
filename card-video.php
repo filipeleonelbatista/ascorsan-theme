18f702f1d0e52326e2e642412de1075ae1c87099
filipe batista <filipe.x@live.com>
2020-04-02 09:02:36 -0300

    atualizando ws

---
+++ card-video.php
@@ -0,0 +1,38 @@
+<!-- Postagem normal -->
+<div class="col-sm-4">
+        <div class="card mb-4 box-shadow shadow">           
+          
+        <a class='titulo-link' href="<?php the_permalink(); ?>"> 
+        <div class="overlay">
+            <?php
+            if (has_post_thumbnail()) {
+                the_post_thumbnail('post-thumbnail', array(
+                'class' => 'card-img-top',
+                'style' => 'height: 155px; width: 100%; display: block; position: relative;',
+                ));
+            } else {
+                echo '<img src="' . get_bloginfo('stylesheet_directory')
+                . '/img/default.png" class="card-img-top" style="height: 155px; width: 100%; display: block; position: relative;" />';
+            }
+            ?>
+            <div class="text-block">
+                <i class="far fa-play-circle"></i>
+            </div>
+          </div>          
+          </a>
+          <div class="card-body">
+            <a class='titulo-link' href="<?php the_permalink(); ?>">
+              <h4><?php the_title(); ?></h4>
+            </a>
+            <p class="card-text">
+              <?php
+                echo substr(get_the_excerpt(), 0,150)." [...]";
+                ?>
+            </p>
+          </div>
+          <div class="card-footer text-muted">
+            <i class="far fa-clock"></i><small>Publicado em: <?php echo get_the_date('d/m/Y'); ?> </small>
+          </div>
+        </div>
+      </div>
+      <!-- FIM Postagem normal -->
---
+++ manutencao.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+
+<section id="content" style="background-color: #e9ecef;">
+  <div class="container" style="background-color: white;">
+    <div class="col-sm-12 pt-2 pb-4">
+      <h1 class="text-center mt-2">Nosso site está passando por alterações!</h1>
+      <p class="text-center mt-4 mb-4">Em breve, tudo voltará ao normal.</p>
+
+      <div class="d-flex justify-content-center text-center">
+          <div class="col-md-4">
+  
+          </div>
+          <div class="col-md-4">
+            <div class="row mt-2">        
+              <a href="https://www.dropbox.com/s/8kkmbs3baaaqxf9/1PropostadeSocio.pdf?dl=0" class="btn btn-sm btn-primary btn-block">Proposta de sócio</a>
+            </div>
+            <div class="row mt-2">
+              <a href="https://www.dropbox.com/s/99ej659e8u496qv/ODONTO2019%20copy.pdf?dl=0" class="btn btn-sm btn-primary btn-block">Adesão ao Plano Odontológico</a>
+            </div>
+            <div class="row mt-2">
+              <a href="https://www.dropbox.com/s/l0aq4c0ccd2m481/Ades%C3%A3o%20Funeral%202019.pdf?dl=0" class="btn btn-sm btn-primary btn-block">Adesão para assistência Funeral</a>
+            </div>
+            <div class="row mt-4">
+              <a href="https://www.dropbox.com/s/3ntd3n7a1tc7ypc/Balan%C3%A7o%20Social%202019.pdf?dl=0" class="btn btn-sm btn-primary btn-block">Balanço Patrimonial 2019</a>
+            </div>
+            <div class="row mt-2">
+              <a href="https://www.dropbox.com/s/zo03o93lrkfq7wq/Parecer%20Coasa.pdf?dl=0" class="btn btn-sm btn-primary btn-block">Parecer Coasa</a>
+            </div>
+            <div class="row mt-2">
+              <a href="https://www.dropbox.com/s/lsbhesieokr9ep4/Parecer%20Conselho%20Fiscal.pdf?dl=" class="btn btn-sm btn-primary btn-block">Parecer Conselho Fiscal</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+  
+          </div>
+      </div>
+
+
+      
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
